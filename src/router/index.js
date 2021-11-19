3ce82e731d13033323dcbbedc8982dc1460ce7b1
andrea_degiovanni <andreadegiovanni@navert.it>
2021-11-19 09:49:07 +0100

    Corretti problemi di webpack

--- src/router/index.js
+++ src/router/index.js
@@ -18,10 +18,10 @@ const GestioneNewsBroker = () => import("@/views/news/GestioneNewsBroker");
 const Documentale = () => import("@/views/documentale/Documentale");
 
 // Views - Formazione
-const AdminFormazione = () => import(/* webpackChunkName: "Commerciale" */"@/views/formazione/AdminFormazione");
-const Formazione = () => import(/* webpackChunkName: "Commerciale" */"@/views/formazione/Formazione");
-const Corso = () => import(/* webpackChunkName: "Commerciale" */"@/views/formazione/Corso");
-const ModificaCorso = () => import(/* webpackChunkName: "Commerciale" */"@/views/formazione/ModificaCorso");
+const AdminFormazione = () => import(/* webpackChunkName: "Formazione" */"@/views/formazione/AdminFormazione");
+const Formazione = () => import(/* webpackChunkName: "Formazione" */"@/views/formazione/Formazione");
+const Corso = () => import(/* webpackChunkName: "Formazione" */"@/views/formazione/Corso");
+const ModificaCorso = () => import(/* webpackChunkName: "Formazione" */"@/views/formazione/ModificaCorso");
 
 // Views - Commerciale
 const AdminCommerciale = () => import(/* webpackChunkName: "Commerciale" */"@/views/commerciale/AdminCommerciale");
