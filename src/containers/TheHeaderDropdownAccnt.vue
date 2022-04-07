1076002b27622366389142daf25597a3fbfb5c2c
Andrea De Giovanni <andrea.degiovanni@gmail.com>
2022-04-07 17:12:55 +0200

    Rilasciata pagina totale accessi alle piattaforme

--- src/containers/TheHeaderDropdownAccnt.vue
+++ src/containers/TheHeaderDropdownAccnt.vue
@@ -116,6 +116,7 @@
       <template #toggler>
         <CHeaderNavLink>
           <div
+           @click="aggiornaSede()"
             id="user_option"
             class="c-avatar"
             v-c-tooltip="{ content: 'OPZIONI', placement: 'bottom-end' }"
@@ -131,52 +132,9 @@
       <CDropdownItem @click="Get_user()">
         <CIcon name="cil-user" /> Profilo
       </CDropdownItem>
-      <CDropdownItem>
-        <CHeaderNavLink
-        to="Accessi_stat">
+      <CDropdownItem to="Accessi_stat" v-if="admin" >
         <i class="far fa-chart-bar"> </i> <span class="pl-1">Accessi</span>
-        </CHeaderNavLink>
       </CDropdownItem>
-      <!-- <CDropdownItem>
-        <CIcon name="cil-bell" /> Updates
-        <CBadge color="info" class="mfs-auto">{{
-          itemsCount
-        }}</CBadge> </CDropdownItem> -->
-
-      <!-- 
-    <CDropdownItem>
-      <CIcon name="cil-envelope-open" /> Messages
-      <CBadge color="success" class="mfs-auto">{{ itemsCount }}</CBadge>
-    </CDropdownItem>
-    <CDropdownItem>
-      <CIcon name="cil-task" /> Tasks
-      <CBadge color="danger" class="mfs-auto">{{ itemsCount }}</CBadge>
-    </CDropdownItem>
-    <CDropdownItem>
-      <CIcon name="cil-comment-square" /> Comments
-      <CBadge color="warning" class="mfs-auto">{{ itemsCount }}</CBadge>
-    </CDropdownItem> -->
-      <!-- <CDropdownHeader
-      tag="div"
-      class="text-center"
-      color="light"
-    >
-      <strong>Settings</strong>
-    </CDropdownHeader>
-    
-    
-    <CDropdownItem>
-      <CIcon name="cil-dollar" /> Payments
-      <CBadge color="secondary" class="mfs-auto">{{ itemsCount }}</CBadge>
-    </CDropdownItem>
-    <CDropdownItem>
-      <CIcon name="cil-file" /> Projects
-      <CBadge color="primary" class="mfs-auto">{{ itemsCount }}</CBadge>
-    </CDropdownItem>
-    <CDropdownDivider/>
-    <CDropdownItem>
-      <CIcon name="cil-shield-alt" /> Lock Account
-    </CDropdownItem> -->
       <CDropdownItem @click="Logout()">
         <!-- <CHeaderNavLink to="login"> -->
         <CIcon :content="$options.logout_ico" /> Logout
@@ -194,6 +152,7 @@ export default {
   logout_ico: cilAccountLogout,
   data() {
     return {
+      admin: false,
       user: [],
       logout_modale: false,
       show_profile: false,
@@ -203,13 +162,17 @@ export default {
   },
 
   methods: {
+    aggiornaSede(){
+      this.admin = JSON.parse(localStorage.getItem("chisono_data")).Is_Sede;
+      console.log("agg_sede");
+    },
     Get_user() {
       this.user = JSON.parse(localStorage.getItem("chisono_data"));
       this.show_profile = true;
     },
 
     Logout() {
-      // // funzione di logout. Viene chiamata la pagina del broker e di energy e dopo 5 secondi viene effettuato il redirect alla login
+      // // funzione di logout. Viene chiamata la pagina del broker e di energy e dopo 2 secondi viene effettuato il redirect alla login
       this.logout_modale = true;
       store.commit("user_logout");
       setTimeout(() => {
@@ -231,5 +194,11 @@ export default {
 }
 .c-icon {
   margin-right: 0.3rem;
+}
+.dropdown-item.active,
+.dropdown-item:active {
+  text-decoration: none;
+  color: #fff !important;
+  background-color: #1f4b6b !important;
 }
 </style>
--- src/views/statistiche/Accessi_stat.vue
+++ src/views/statistiche/Accessi_stat.vue
@@ -1,6 +1,160 @@
 <template>
-  <CContainer>
+  <CContainer class="text-center" style="background-color: white;">
+    <p class="display-3 mb-5">
+        Accessi alle piattaforme
+      </p>
+    <CRow style="border-top: 1px solid lightgray;">
+      <CCol col="12" sm="2" >
+        <h1>Totali</h1>
+        <cite>dal 7/4/2022</cite>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="broker"
+          :rightHeader="accessi_totali.Broker_totale"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Broker_unici"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="fas fa-car-alt fa-3x"></i>
+            <br />
+            Broker
+          </span>
+        </CWidgetBrand>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="rami"
+          :rightHeader="accessi_totali.Rami_totale"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Rami_unici"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="fas fa-user-tie fa-3x"></i>
+            <br />
+            Professionisti
+          </span>
+        </CWidgetBrand>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="energy"
+          :rightHeader="accessi_totali.Energy_totale"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Energy_unici"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="far fa-lightbulb fa-3x"></i>
+            <br />
+            Gas & Luce
+          </span>
+        </CWidgetBrand>
+      </CCol>
+    </CRow>
+    <CRow style="border-top: 1px solid lightgray; border-bottom: 1px solid lightgray">
+      <CCol col="12" sm="2">
+       <h1>Mese corrente</h1>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="broker"
+          :rightHeader="accessi_totali.Broker_totale_m"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Broker_unici_m"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="fas fa-car-alt fa-3x"></i>
+            <br />
+            Broker
+          </span>
+        </CWidgetBrand>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="rami"
+          :rightHeader="accessi_totali.Rami_totale_m"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Rami_unici_m"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="fas fa-user-tie fa-3x"></i>
+            <br />
+            Professionisti
+          </span>
+        </CWidgetBrand>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="energy"
+          :rightHeader="accessi_totali.Energy_totale_m"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Energy_unici_m"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="far fa-lightbulb fa-3x"></i>
+            <br />
+            Gas & Luce
+          </span>
+        </CWidgetBrand>
+      </CCol>
+    </CRow>
     <CRow>
+      <CCol col="12" sm="2">
+        <h1>Oggi</h1> 
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="broker"
+          :rightHeader="accessi_totali.Broker_totale_d"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Broker_unici_d"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="fas fa-car-alt fa-3x"></i>
+            <br />
+            Broker
+          </span>
+        </CWidgetBrand>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="rami"
+          :rightHeader="accessi_totali.Rami_totale_d"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Rami_unici_d"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="fas fa-user-tie fa-3x"></i>
+            <br />
+            Professionisti
+          </span>
+        </CWidgetBrand>
+      </CCol>
+      <CCol col="12" sm="3">
+        <CWidgetBrand
+          color="energy"
+          :rightHeader="accessi_totali.Energy_totale_d"
+          rightFooter="Totali"
+          :leftHeader="accessi_totali.Energy_unici_d"
+          leftFooter="univoci"
+        >
+          <span class="py-3">
+            <i class="far fa-lightbulb fa-3x"></i>
+            <br />
+            Gas & Luce
+          </span>
+        </CWidgetBrand>
+      </CCol>
+    </CRow>
+    <!-- <CRow>
       <vc-date-picker
         v-model="range"
         color="blue"
@@ -12,381 +166,15 @@
     <CRow v-if="range.start && range.end">
       Periodo selezionato:<br />dal {{ range.start.toLocaleDateString() }} al
       {{ range.end.toLocaleDateString() }}
-    </CRow>
-    <CRow>
-      <CCol :xs="4">
-        <CWidgetStatsA class="mb-3" color="primary">
-          <template #value
-            >26K
-            <span class="fs-6 fw-normal">
-              (-12.4% <CIcon icon="cil-arrow-bottom" />)
-            </span>
-          </template>
-          <template #title>Users</template>
-          <template #action>
-            <CDropdown placement="bottom-end">
-              <CDropdownToggle
-                color="transparent"
-                class="p-0 text-white"
-                :caret="false"
-              >
-                <CIcon icon="cil-options" class="text-high-emphasis-inverse" />
-              </CDropdownToggle>
-              <CDropdownMenu>
-                <CDropdownItem href="#">Action</CDropdownItem>
-                <CDropdownItem href="#">Another action</CDropdownItem>
-                <CDropdownItem href="#">Something else here</CDropdownItem>
-              </CDropdownMenu>
-            </CDropdown>
-          </template>
-          <template #chart>
-            <CChart
-              type="line"
-              class="mt-3 mx-3"
-              style="height: 70px"
-              :data="{
-                labels: [
-                  'January',
-                  'February',
-                  'March',
-                  'April',
-                  'May',
-                  'June',
-                  'July',
-                ],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    backgroundColor: 'transparent',
-                    borderColor: 'rgba(255,255,255,.55)',
-                    pointBackgroundColor: '#321fdb',
-                    data: [65, 59, 84, 84, 51, 55, 40],
-                  },
-                ],
-              }"
-              :options="{
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                maintainAspectRatio: false,
-                scales: {
-                  x: {
-                    grid: {
-                      display: false,
-                      drawBorder: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                  y: {
-                    min: 30,
-                    max: 89,
-                    display: false,
-                    grid: {
-                      display: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                },
-                elements: {
-                  line: {
-                    borderWidth: 1,
-                    tension: 0.4,
-                  },
-                  point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                  },
-                },
-              }"
-            />
-          </template>
-        </CWidgetStatsA>
-      </CCol>
-      <CCol :xs="4">
-        <CWidgetStatsA class="mb-3" color="info">
-          <template #value
-            >$6.200
-            <span class="fs-6 fw-normal">
-              (40.9% <CIcon icon="cil-arrow-top" />)
-            </span>
-          </template>
-          <template #title>Income</template>
-          <template #action>
-            <CDropdown placement="bottom-end">
-              <CDropdownToggle
-                color="transparent"
-                class="p-0 text-white"
-                :caret="false"
-              >
-                <CIcon icon="cil-options" class="text-high-emphasis-inverse" />
-              </CDropdownToggle>
-              <CDropdownMenu>
-                <CDropdownItem href="#">Action</CDropdownItem>
-                <CDropdownItem href="#">Another action</CDropdownItem>
-                <CDropdownItem href="#">Something else here</CDropdownItem>
-              </CDropdownMenu>
-            </CDropdown>
-          </template>
-          <template #chart>
-            <CChart
-              type="line"
-              class="mt-3 mx-3"
-              style="height: 70px"
-              :data="{
-                labels: [
-                  'January',
-                  'February',
-                  'March',
-                  'April',
-                  'May',
-                  'June',
-                  'July',
-                ],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    backgroundColor: 'transparent',
-                    borderColor: 'rgba(255,255,255,.55)',
-                    pointBackgroundColor: '#39f',
-                    data: [1, 18, 9, 17, 34, 22, 11],
-                  },
-                ],
-              }"
-              :options="{
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                maintainAspectRatio: false,
-                scales: {
-                  x: {
-                    grid: {
-                      display: false,
-                      drawBorder: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                  y: {
-                    min: -9,
-                    max: 39,
-                    display: false,
-                    grid: {
-                      display: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                },
-                elements: {
-                  line: {
-                    borderWidth: 1,
-                  },
-                  point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                  },
-                },
-              }"
-            />
-          </template>
-        </CWidgetStatsA>
-      </CCol>
-      <CCol :xs="4">
-        <CWidgetStatsA class="mb-3" color="warning">
-          <template #value
-            >2.49%
-            <span class="fs-6 fw-normal">
-              (84.7% <CIcon icon="cil-arrow-top" />)
-            </span>
-          </template>
-          <template #title>Conversion Rate</template>
-          <template #action>
-            <CDropdown placement="bottom-end">
-              <CDropdownToggle
-                color="transparent"
-                class="p-0 text-white"
-                :caret="false"
-              >
-                <CIcon icon="cil-options" class="text-high-emphasis-inverse" />
-              </CDropdownToggle>
-              <CDropdownMenu>
-                <CDropdownItem href="#">Action</CDropdownItem>
-                <CDropdownItem href="#">Another action</CDropdownItem>
-                <CDropdownItem href="#">Something else here</CDropdownItem>
-              </CDropdownMenu>
-            </CDropdown>
-          </template>
-          <template #chart>
-            <CChart
-              type="line"
-              class="mt-3"
-              style="height: 70px"
-              :data="{
-                labels: [
-                  'January',
-                  'February',
-                  'March',
-                  'April',
-                  'May',
-                  'June',
-                  'July',
-                ],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    backgroundColor: 'rgba(255,255,255,.2)',
-                    borderColor: 'rgba(255,255,255,.55)',
-                    data: [78, 81, 80, 45, 34, 12, 40],
-                    fill: true,
-                  },
-                ],
-              }"
-              :options="{
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                maintainAspectRatio: false,
-                scales: {
-                  x: {
-                    display: false,
-                  },
-                  y: {
-                    display: false,
-                  },
-                },
-                elements: {
-                  line: {
-                    borderWidth: 2,
-                    tension: 0.4,
-                  },
-                  point: {
-                    radius: 0,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                  },
-                },
-              }"
-            />
-          </template>
-        </CWidgetStatsA>
-      </CCol>
-      <CCol :xs="4">
-        <CWidgetStatsA class="mb-3" color="danger">
-          <template #value
-            >44K
-            <span class="fs-6 fw-normal">
-              (-23.6% <CIcon icon="cil-arrow-bottom" />)
-            </span>
-          </template>
-          <template #title>Sessions</template>
-          <template #action>
-            <CDropdown placement="bottom-end">
-              <CDropdownToggle
-                color="transparent"
-                class="p-0 text-white"
-                :caret="false"
-              >
-                <CIcon icon="cil-options" class="text-high-emphasis-inverse" />
-              </CDropdownToggle>
-              <CDropdownMenu>
-                <CDropdownItem href="#">Action</CDropdownItem>
-                <CDropdownItem href="#">Another action</CDropdownItem>
-                <CDropdownItem href="#">Something else here</CDropdownItem>
-              </CDropdownMenu>
-            </CDropdown>
-          </template>
-          <template #chart>
-            <CChart
-              type="bar"
-              class="mt-3 mx-3"
-              style="height: 70px"
-              :data="{
-                labels: [
-                  'January',
-                  'February',
-                  'March',
-                  'April',
-                  'May',
-                  'June',
-                  'July',
-                  'August',
-                  'September',
-                  'October',
-                  'November',
-                  'December',
-                  'January',
-                  'February',
-                  'March',
-                  'April',
-                ],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    backgroundColor: 'rgba(255,255,255,.2)',
-                    borderColor: 'rgba(255,255,255,.55)',
-                    data: [
-                      78, 81, 80, 45, 34, 12, 40, 85, 65, 23, 12, 98, 34, 84,
-                      67, 82,
-                    ],
-                    barPercentage: 0.6,
-                  },
-                ],
-              }"
-              :options="{
-                maintainAspectRatio: false,
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                scales: {
-                  x: {
-                    grid: {
-                      display: false,
-                      drawTicks: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                  y: {
-                    grid: {
-                      display: false,
-                      drawBorder: false,
-                      drawTicks: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                },
-              }"
-            />
-          </template>
-        </CWidgetStatsA>
-      </CCol>
-    </CRow>
+    </CRow> -->
   </CContainer>
 </template>
 <script>
-// import CWidgetStatsA from '@coreui/vue/src/components/widgets/CWidgetStatsA'
+import axios from "axios";
 export default {
-   data() {
+  data() {
     return {
+      accessi_totali:[],
       attrs: [
         {
           key: "today",
@@ -398,7 +186,43 @@ export default {
         start: "",
         end: "",
       },
+      contBroker: "400",
+      contRami: "20",
+      contEnergy: "100",
     };
   },
+  mounted(){
+    this.get_accessi_totali();
+  },
+  methods:{
+     async get_accessi_totali() {
+      // Chiamata per recuperare il totale degli accessi alle piattaforme
+      try {
+        await axios
+          .post(
+            this.$custom_json.base_url +
+              this.$custom_json.api_url +
+              this.$custom_json.ep_api.get_accessi_totali
+          )
+          .then((response) => {
+            this.accessi_totali= response.data;
+           console.log(this.accessi_totali.Broker_totale);
+          });
+      } catch (error) {
+        console.log("errore: " + error);
+      }
+    },
+  }
 };
 </script>
+<style>
+.bg-broker {
+  background-color: #1f4b6b !important;
+}
+.bg-rami {
+  background-color: #607d8b !important;
+}
+.bg-energy {
+  background-color: green !important;
+}
+</style>
