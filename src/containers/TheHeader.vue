f91dee42b09237106cc15c2be0de0062edb69ba9
Andrea De Giovanni <andrea.degiovanni@gmail.com>
2022-04-08 15:34:35 +0200

    Header versione mobile con logo cliccabile

--- src/containers/TheHeader.vue
+++ src/containers/TheHeader.vue
@@ -1,45 +1,73 @@
 <template>
-
-  <CHeader fixed with-subheader light class="d-flex justify-content-between fixed-nav">
- 
+  <!-- <CHeader fixed with-subheader light class="d-flex justify-content-between fixed-nav"> -->
+  <CHeader
+    fixed
+    with-subheader
+    light
+    class="c-header c-header-light c-header-fixed c-header-with-subheader"
+  >
     <CHeaderNav class="d-md-down-none mr-auto">
       <CHeaderNavLink to="/dashboard" exact>
-      <CHeaderNavItem class="px-3">
-       <img v-if="this.$route.path != '/dashboard'" src="/img/logo_abyway.png" width="200px" v-c-tooltip="{content:'Clicca qui per tornare alla HOME', placement:'right'}">
-       <img v-else src="/img/logo_abyway.png" width="200px" >
-      </CHeaderNavItem>
+        <CHeaderNavItem class="px-3">
+          <img
+            v-if="this.$route.path != '/dashboard'"
+            src="/img/logo_abyway.png"
+            width="200px"
+            v-c-tooltip="{
+              content: 'Clicca qui per tornare alla HOME',
+              placement: 'right',
+            }"
+          />
+          <img v-else src="/img/logo_abyway.png" width="200px" />
+        </CHeaderNavItem>
       </CHeaderNavLink>
-      
     </CHeaderNav>
-    
+    <a
+      href="dashboard"
+      class="c-header-brand mx-auto d-lg-none router-link-active"
+    >
+      <img
+        v-if="this.$route.path != '/dashboard'"
+        src="/img/logo_abyway.png"
+        style="scale: 60%"
+        v-c-tooltip="{
+          content: 'Clicca qui per tornare alla HOME',
+          placement: 'right',
+        }"
+      />
+      <img v-else src="/img/logo_abyway.png" style="scale:60%" />
+    </a>
     <CHeaderNav class="mr-4">
-     
-      <CHeaderNavItem class="d-md-down-none mx-2" v-c-tooltip="{content:'Clicca qui per tornare INDIETRO', placement:'bottom-end'}" >
-        <button @click="$router.go(-1)"  >
-        <CHeaderNavLink v-show="this.$route.path != '/dashboard'" >
-          <CIcon id="icona_indietro" :content="$options.back_ico" size="2xl"/>
-        </CHeaderNavLink>
+      <CHeaderNavItem
+        class="d-md-down-none mx-2"
+        v-c-tooltip="{
+          content: 'Clicca qui per tornare INDIETRO',
+          placement: 'bottom-end',
+        }"
+      >
+        <button @click="$router.go(-1)">
+          <CHeaderNavLink v-show="this.$route.path != '/dashboard'">
+            <CIcon
+              id="icona_indietro"
+              :content="$options.back_ico"
+              size="2xl"
+            />
+          </CHeaderNavLink>
         </button>
       </CHeaderNavItem>
-     
-       <!-- <CHeaderNavItem class="d-md-down-none mx-2">
+
+      <!-- <CHeaderNavItem class="d-md-down-none mx-2">
         <CHeaderNavLink>
           <CIcon name="cil-envelope-open" />
         </CHeaderNavLink>
       </CHeaderNavItem> -->
       <TheHeaderDropdownAccnt />
     </CHeaderNav>
-
   </CHeader>
-
-
-
-
 </template>
 
 <script>
-  
-  import { cilArrowCircleLeft   } from "@coreui/icons";
+import { cilArrowCircleLeft } from "@coreui/icons";
 import TheHeaderDropdownAccnt from "./TheHeaderDropdownAccnt";
 
 export default {
