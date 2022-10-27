84379842bae38fb192eead673e09c1519f3297d8
Andrea De Giovanni <andreadegiovanni@navert.it>
2022-10-27 17:22:39 +0200

    Completati filtri di ricerca ed aggiunto link per tornare alla home sul logo

--- src/views/formazione/Formazione.vue
+++ src/views/formazione/Formazione.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="pt-2" :class="corsi_filtrati.length && 'formazione_style'">
-    <div>
-      <div v-if="corsi_filtrati.length == 0" class="col-12 text-center">
-        <h1 class="display-3 mr-4 pb-5">Non sono presenti corsi</h1>
-      </div>
-    </div>
+    
     <div v-if="corsi_filtrati.length > 0" class="row display-4 pb-3">
       <div class="col-12 text-center">Tutti i corsi a te riservati</div>
     </div>
     <p
       v-if="corsi_filtrati.length > 0"
       class="text-center"
-      style="font-weight: 600; font-variant: small-caps"
+      style="font-weight: 600; font-variant: small-caps; margin-bottom: 0px !important; "
     >
       Totale corsi presenti: {{ corsi.length }}
     </p>
     <div id="filtro_ricerca" class="mt-2 mx-5 riquadro row">
-      <div class="text-left col-sm-4">
+      <div class="text-left col-sm-3">
         <strong> <i class="fas fa-filter"></i> Filtra per categoria:</strong>
         <treeselect
           v-model="filtroCat"
@@ -27,7 +23,7 @@
           placeholder="Seleziona una categoria"
         />
       </div>
-      <div class="text-left col-sm-4">
+      <div class="text-left col-sm-3">
         <strong>Ricerca: </strong>
         <input
           v-model="ricercaTxt"
@@ -41,15 +37,31 @@
             border-top: 0 !important;
             background-color: unset !important;
             border-radius: 0 !important;
-            padding-left: 0 !important;
+            padding-left: 3px !important;
             margin-bottom: 0 !important;
           "
-        /><i class="fas fa-search fa-2x"></i>
+        /><i class="fas fa-search"></i>
       </div>
-      <div class="text-right col-sm-4" v-if="admin">
+      <div class="text-left col-sm-3">
+        <strong> <i class="fas fa-filter"></i> Filtra per anno:</strong>
+        <treeselect
+          v-model="filtroAnno"
+          class="col-sm-5 ml-2 filtro"
+          :multiple="false"
+          :options="anni_select"
+          :max-height="300"
+          placeholder="Seleziona un anno"
+        />
+      </div>
+      <div class="text-right col-sm-3" v-if="admin">
         <CLink to="AdminFormazione" style="color: #ef7918 !important"
           ><CIcon name="cil-settings" /><strong> Gestione contenuti</strong>
         </CLink>
+      </div>
+    </div>
+    <div>
+      <div v-if="corsi_filtrati.length == 0" class="col-12 text-center">
+        <h1 class="display-4 mr-4 pb-5">Non sono presenti corsi</h1>
       </div>
     </div>
     <div v-if="corsi_filtrati.length > 0" class="row">
@@ -157,6 +169,16 @@ export default {
       lista_nuovi: [],
       filtroCat: null,
       categorie: [],
+      anni_select: [],
+
+      // anni_select: [
+      //   {
+      //     id: "2022",
+      //     label: "2022",
+      //   },
+      //   { id: "2023", label: "2023" },
+      // ],
+      filtroAnno: null,
       ricercaTxt: "",
     };
   },
@@ -172,12 +194,19 @@ export default {
             .includes(this.ricercaTxt.toUpperCase());
         });
       }
+      // Filtro per categoria
       if (!this.filtroCat) {
         // console.log("niente filtro");
         tempCorsi = tempCorsi.slice(0);
       } else {
         // console.log("filtro");
         tempCorsi = tempCorsi.filter((a) => a.id_categoria == this.filtroCat);
+      }
+      // Filtro per anno di pubblicazione
+      if (this.filtroAnno) {
+        tempCorsi = tempCorsi.filter((a) => {
+          return a.data_ins.substr(0, 4).includes(this.filtroAnno);
+        });
       }
       return tempCorsi;
     },
@@ -186,7 +215,7 @@ export default {
     this.get_lista_corsi();
     this.getToSee();
     this.get_categorie();
-    this.filtra_risultati();
+    // this.filtra_risultati();
   },
   methods: {
     async getToSee() {
@@ -240,11 +269,29 @@ export default {
           return { ...item, id };
         });
         // console.log("corsi "+ this.corsi);
-        this.corsi_filtrati = this.corsi.slice(0);
+        // this.corsi_filtrati = this.corsi.slice(0);
         // console.log("corsi filtrati " + this.corsi_filtrati);
       } catch (error) {
         console.log("impossibile accedere al cloud");
       }
+      // Genero l'elenco degli anni disponibili per popolare la select del filtro per anno
+      // this.anni_select = this.corsi.map((event) => {
+      //   return {
+      //     id: new Date(event.data_ins).getFullYear(),
+      //     label: new Date(event.data_ins).getFullYear(),
+      //   };
+      // });
+      const result = [
+        ...new Set(
+          this.corsi.map((event) => new Date(event.data_ins).getFullYear())
+        ),
+      ];
+      result.forEach((element) => {
+        // console.log(element);
+        this.anni_select.push({ id: element, label: element });
+        // console.log(this.anni_select);
+      });
+      // console.log("filtrato= ", result);
     },
     async get_categorie() {
       // Chiamata per recuperare la lista delle categorie
@@ -335,13 +382,15 @@ export default {
   border: 1px solid lightgray;
   border-radius: 10px;
   padding: 0.5rem;
+  background-color: aliceblue;
+  margin-bottom: 3px !important;
 }
 .views {
   position: absolute;
   top: auto;
-  left: 3px;
+  left: 5px;
   right: auto;
-  color: lightgray;
+  color: rgb(164, 160, 160);
   font-size: 0.7rem;
 }
 .filtro {
@@ -356,7 +405,7 @@ export default {
   border-top: 0 !important;
   background-color: unset !important;
   border-radius: 0 !important;
-  padding-left: 0 !important;
+  padding-left: 3px !important;
   margin-bottom: 0 !important;
 }
 </style>
