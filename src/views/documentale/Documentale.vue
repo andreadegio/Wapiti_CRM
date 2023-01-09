ac8596a89532fa0ba75f8d9a40a239ed6b325365
Andrea De Giovanni <andreadegiovanni@navert.it>
2023-01-09 11:03:01 +0100

    Esportazione cataloghi

--- src/views/documentale/Documentale.vue
+++ src/views/documentale/Documentale.vue
@@ -391,12 +391,23 @@
                           </tr>
                         </tbody>
                       </table>
+                      <div class="text-left">
+                        <CButton
+                          color="primary"
+                          style="color: white; padding: 8px"
+                          square
+                          size="sm"
+                          @click="download_excel_auto()"
+                          ><i class="fas fa-file-excel fa-2x"></i> &nbsp;
+                          Esporta Catalogo</CButton
+                        >
+                      </div>
                     </div>
                   </div>
                 </span>
               </div>
               <!-- SETTORE NON AUTO -->
-              <div class="pt-5" v-if="settore == 'RAMI'">
+              <div class="pt-2" v-if="settore == 'RAMI'">
                 <span>
                   <div class="pt-0">
                     <div>
@@ -454,6 +465,17 @@
                         </tbody>
                       </table>
                     </div>
+                    <div class="text-left">
+                      <CButton
+                        color="primary"
+                        style="color: white; padding: 8px"
+                        square
+                        size="sm"
+                        @click="download_excel_rami()"
+                        ><i class="fas fa-file-excel fa-2x"></i> &nbsp; Esporta
+                        Catalogo</CButton
+                      >
+                    </div>
                   </div>
                 </span>
               </div>
@@ -469,7 +491,7 @@
               - Al momento non ci sono documenti disponibili -
             </div>
             <!-- DATA TABLE PER ORGANIGRAMMA ABY -->
-            <div class="pt-5" v-if="settore === 'ORGANIGRAMMA ABY BROKER'">
+            <div class="pt-2" v-if="settore === 'ORGANIGRAMMA ABY BROKER'">
               <CDataTable
                 id="int_table"
                 ref="tabella_ORGANIGRAMMA ABY"
@@ -505,7 +527,7 @@
             </div>
             <!-- DATA TABLE PER CIRCOLARI -->
             <div
-              class="pt-5"
+              class="pt-2"
               v-if="
                 (settore === 'CIRCOLARI INTERNE' && vuoto == false) ||
                 (settore === 'CIRCOLARI OPERATIVE' && vuoto == false)
@@ -546,7 +568,7 @@
             </div>
             <!-- DATA TABLE PER DOCUMENTI INTERMEDIARIO -->
             <div
-              class="pt-5"
+              class="pt-2"
               v-if="
                 settore === 'INTERMEDIARIO' ||
                 (settore === 'PRECONTRATTUALE' && vuoto == false)
@@ -583,7 +605,7 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER EMITTENTI -->
-            <div class="pt-3" v-if="settore === 'INTERMEDIARI EMITTENTI'">
+            <div class="pt-2" v-if="settore === 'INTERMEDIARI EMITTENTI'">
               <div class="row">
                 <div class="col-sm-4"></div>
                 <div class="col-sm-4 text-center"></div>
@@ -724,7 +746,7 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER PROPONENTI -->
-            <div class="pt-1" v-if="settore === 'INTERMEDIARI PROPONENTI'">
+            <div class="pt-2" v-if="settore === 'INTERMEDIARI PROPONENTI'">
               <p class="text-right">
                 Totale Intermediari Proponenti: <b>{{ files.length }}</b>
               </p>
@@ -855,7 +877,7 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER UNITA' OPERATIVE -->
-            <div class="pt-1" v-if="settore === 'UNITA OPERATIVE'">
+            <div class="pt-2" v-if="settore === 'UNITA OPERATIVE'">
               <p class="text-right">
                 Totale Unità Operative: <b>{{ files.length }}</b>
               </p>
@@ -940,8 +962,19 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER RCA -->
-            <div class="pt-5" v-if="settore === 'RC AUTO'">
-              <button @click="download_excel()">Scarica</button>
+            <div class="pt-2" v-if="settore === 'RC AUTO'">
+              <!-- <div class="text-center">
+                <CButton
+                  color="primary"
+                  style="color: white; padding: 8px"
+                  square
+                  size="sm"
+                  @click="download_excel()"
+                  ><i class="fas fa-file-excel fa-2x"></i> &nbsp; Esporta
+                  Risultati</CButton
+                >
+              </div> -->
+
               <CDataTable
                 id="rc_table"
                 :items="files"
@@ -1001,7 +1034,7 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER SERVIZI NON ASSICURATIVI -->
-            <div class="pt-5" v-if="settore === 'SERVIZI NON ASSICURATIVI'">
+            <div class="pt-2" v-if="settore === 'SERVIZI NON ASSICURATIVI'">
               <CDataTable
                 id="altre_table"
                 :items="files"
@@ -1045,7 +1078,7 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER ALTRE GARANZIE -->
-            <div class="pt-5" v-if="settore === 'ALTRE GARANZIE'">
+            <div class="pt-2" v-if="settore === 'ALTRE GARANZIE'">
               <CDataTable
                 id="altre_table"
                 ref="tabella_doc"
@@ -1106,7 +1139,7 @@
               </CDataTable>
             </div>
             <!-- DATA TABLE PER NON AUTO -->
-            <div class="pt-5" v-if="settore === 'RAMI_DATA' && !vuoto">
+            <div class="pt-2" v-if="settore === 'RAMI_DATA' && !vuoto">
               <CDataTable
                 :itemsPerPage="20"
                 id="rami_table"
@@ -1194,7 +1227,7 @@
   </CContainer>
 </template>
 <script>
-import  { jsontoexcel }  from "vue-table-to-excel";
+import { jsontoexcel } from "vue-table-to-excel";
 
 import axios from "axios";
 import {
@@ -1563,15 +1596,6 @@ export default {
   },
   data() {
     return {
-      json_excel: {
-        data: [
-          { name: "Tom", phone: "+321321", email: "pippo" },
-          { name: "Jerry", phone: "+321321", email: "pippo" },
-          { name: "Pippo", phone: "+321321", email: "pippo" },
-        ],
-        head: ["name", "phone", "email"],
-        fileName: "prova_download.csv",
-      },
       json_prodotti: [],
       array_prodotti_rami: [],
       lista_sub_prod: [],
@@ -1714,10 +1738,114 @@ export default {
     }
   },
   methods: {
+    download_excel_auto() {
+      let head = [
+        "ID Compagnia",
+        "Tipo Prodotto",
+        "Denominazione Prodotto",
+        "POG",
+        "Set Informativo",
+      ];
+      let fileName = "Catalogo Prodotti Auto.csv";
+      let datiExcel = [];
+      let pog = "";
+      let setinfo = "";
+      let elenco;
+      for (var i in this.array_link) {
+        if (this.array_link[i].SLUG == "RC_AUTO" || this.array_link[i].SLUG == "ALTRE_GARANZIE" || this.array_link[i].SLUG == "ALTRI_SERVIZI" ) {
+         elenco = this.array_link[i].FILE;
+
+         elenco.forEach(function (item) {
+        item.Pog != "" ? (pog = "SI") : (pog = "NO");
+        item.Nomefile != "" ? (setinfo = "SI") : (setinfo = "NO");
+        datiExcel.push({
+          ["idCompagnia"]: item.idCompagnia,
+          ["Tipo"]: item.Tipo,
+          ["Prodotto"]: item.Descrizione,
+          ["Pog"]: pog,
+          ["SetInformativo"]: setinfo,
+        });
+      });
+          
+        }
+      }
+
+      jsontoexcel.getXlsx(datiExcel, head, fileName);
+    },
     download_excel() {
-      const { data, head, fileName } = this.json_excel;
-      console.log(data);
-      jsontoexcel.getXlsx(data, head, fileName);
+      let head = [
+        "ID Compagnia",
+        "Denominazione Prodotto",
+        "POG",
+        "Set Informativo",
+      ];
+      let fileName = "Catalogo RC Auto.csv";
+      let datiExcel = [];
+      let pog = "";
+      let setinfo = "";
+      this.files.forEach(function (item) {
+        item.Pog != "" ? (pog = "SI") : (pog = "NO");
+        item.Nomefile != "" ? (setinfo = "SI") : (setinfo = "NO");
+        datiExcel.push({
+          ["idCompagnia"]: item.idCompagnia,
+          ["Prodotto"]: item.Descrizione,
+          ["Pog"]: pog,
+          ["SetInformativo"]: setinfo,
+        });
+      });
+
+      jsontoexcel.getXlsx(datiExcel, head, fileName);
+    },
+
+    async download_excel_rami() {
+      let head = [
+        "ID Compagnia",
+        "Tipo",
+        "Denominazione Prodotto",
+        "POG",
+        "Set Informativo",
+      ];
+      let fileName = "Catalogo Prodotti Altri Rami.csv";
+      let datiExcel = [];
+      let pog = "";
+      let setinfo = "";
+      // recupero il catalogo completo chiamando il servizio con idTipo non valorizzato
+      var elenco = [];
+      var config = {
+        method: "post",
+        url:
+          this.$custom_json.servizi_broker +
+          this.$custom_json.ep_broker.Documentale_AltriRamiCatalogoProdotti,
+        headers: {
+          userID: localStorage.getItem("userID"),
+          anagraficaID: localStorage.getItem("anagraficaID"),
+          unitaoperativaID: localStorage.getItem("unitaoperativaID"),
+          idTipo: "",
+        },
+      };
+      await axios(config)
+        .then(function (response) {
+          elenco = response.data;
+        })
+        .catch(function (error) {
+          elenco = [];
+          console.log(error);
+        });
+      // console.log(elenco);
+      elenco.forEach(function (item) {
+        item.Pog != "" ? (pog = "SI") : (pog = "NO");
+        item.Nomefile != "" ? (setinfo = "SI") : (setinfo = "NO");
+        datiExcel.push({
+          ["idCompagnia"]: item.idCompagnia,
+          ["Tipo"]: item.Tipo,
+          ["Prodotto"]: item.Descrizione,
+          ["Pog"]: pog,
+          ["SetInformativo"]: setinfo,
+        });
+      });
+      // console.log(datiExcel);
+
+      jsontoexcel.getXlsx(datiExcel, head, fileName);
     },
 
     reset_pagination() {
