127ee7f787e57d5ad4494059d43632f8d6075f5e
Andrea De Giovanni <andreadegiovanni@navert.it>
2024-05-02 10:45:49 +0200

    controllo eventi sul video

--- src/views/e-learning/corsi.vue
+++ src/views/e-learning/corsi.vue
@@ -4,29 +4,48 @@
             <h1 class="text-center">Corso</h1>
         </v-app-bar>
         <v-main>
-
             <v-row>
                 <v-col cols="9" style="background-color: blue; height: 100%;">
-                    <v-row style="background-color: red; display: flex;  padding: 1rem;"> Video
-
+                    <v-row style="background-color: red; display: flex;  padding: 1rem;">
+                        Video
+                        <video id="videoPlayer" controls disabled>
+                            <source src="video/accademy/dashboard.mp4" type="video/mp4">
+                            Il tuo browser non supporta il tag video.
+                        </video>
                     </v-row>
-                    <v-row style="background-color: green;  padding: 1rem;">Descrizione
-
+                    <v-row style="background-color: green;  padding: 1rem;">
+                        Descrizione
                     </v-row>
                 </v-col>
-
-                <v-col cols="3" id="sidebar-corso">Elenco
-
+                <v-col cols="3" id="sidebar-corso">
+                    Elenco
                 </v-col>
             </v-row>
-
-
         </v-main>
     </v-app>
-
 </template>
 <script>
+export default {
+    name: 'MainLayout',
+    mounted() {
+        const videoPlayer = document.getElementById('videoPlayer');
 
+        videoPlayer.addEventListener('play', () => {
+            // Traccia l'inizio della riproduzione del video
+            console.log("video avviato");
+        });
+
+        videoPlayer.addEventListener('pause', () => {
+            // Traccia la pausa del video
+            console.log("video in pausa");
+        });
+
+        videoPlayer.addEventListener('ended', () => {
+            // Traccia la fine della riproduzione del video
+            console.log("fine del video");
+        });
+    }
+}
 </script>
 <style scoped>
 #sidebar-corso {
